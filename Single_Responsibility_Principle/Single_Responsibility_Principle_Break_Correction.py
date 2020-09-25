5f82cfe2895d3a638bc2fbc95e8a698c0059a6be
Hiral Amodia <amodia.hiral@gmail.com>
2020-09-25 11:22:30 +0530

    Deleted unwanted code

--- Single_Responsibility_Principle/Single_Responsibility_Principle_Break_Correction.py
+++ Single_Responsibility_Principle/Single_Responsibility_Principle_Break_Correction.py
@@ -17,14 +17,6 @@ class TelephoneDirectory:
   def lookup_number(self, name):
     return self.telephonedirectory[name]
  
-  def save_to_file(self, file_name, location):
-    #code to save the contents of telephonedirectory dictionary to the file
-    pass
-
-  def persist_to_database(self, database_details):
-    #code to persist the contents of telephonedirectory dictionary to database
-    pass
-
   def __str__(self):
     ret_dct = ""
     for key, value in self.telephonedirectory.items():
