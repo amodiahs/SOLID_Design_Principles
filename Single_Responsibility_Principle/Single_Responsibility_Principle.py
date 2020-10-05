49e917589b74deb4b0d9913281451c85cc51e473
Hiral Amodia <amodia.hiral@gmail.com>
2020-10-05 12:36:44 +0530

    Update Single_Responsibility_Principle.py
    Updated with proper commenting.

--- Single_Responsibility_Principle/Single_Responsibility_Principle.py
+++ Single_Responsibility_Principle/Single_Responsibility_Principle.py
@@ -1,6 +1,15 @@
 # Single Responsibility Principle (aka Separation Of Concerns)
 # A class should have only one primary responsibility and reason to change
 
+'''
+Let’s take an example of a Telephone Directory application. 
+We are designing a Telephone Directory and that contains a TelephoneDirectory Class which is supposed to handle primary responsibility 
+of maintaining Telephone Directory entries, i. e Telephone numbers and Names of the entities to which the Telephone Numbers belong. 
+Thus, the operations that this class is expected to perform are adding a new entry (Name and Telephone Number), delete an existing entry, 
+change a Telephone Number assigned to a particular entity Name, and provide a lookup that returns Telephone Number assigned to a 
+particular entity Name.
+'''
+
 class TelephoneDirectory:
   def __init__(self):
     self.telephonedirectory = {}
