081eb9da6fb6c18531d8dd1ec23d6957e1b201e5
Hiral Amodia <amodia.hiral@gmail.com>
2020-10-06 13:22:54 +0530

    Update Liskov_Substitution_Principle_Break.py
    Updated for good coding practices

--- Liskov_Substitution_Principle/Liskov_Substitution_Principle_Break.py
+++ Liskov_Substitution_Principle/Liskov_Substitution_Principle_Break.py
@@ -1,16 +1,22 @@
-#Breaking - Liskov Substitution Principle 
-class car():
+# Liskov Substitution Principle
+# Objects in a program should be replaceable with instances of their subtypes without altering the correctness of that program 
+'''
+As we can see here, we are trying to loop through a list of car objects. And here we break the Liskov Substitution principle as 
+we cannot replace Super type Car’s objects with objects of Sub type PetrolCar in the function written to find Red cars. 
+'''
+
+class Car():
   def __init__(self, type):
     self.type = type
 
-class petrol_car(car):
+class PetrolCar(Car):
   def __init__(self, type):
     self.type = type
 
-car = car("SUV")
+car = Car("SUV")
 car.properties = {"Color": "Red", "Gear": "Auto", "Capacity": 6}
 
-petrol_car = petrol_car("Sedan")
+petrol_car = PetrolCar("Sedan")
 petrol_car.properties = ("Blue", "Manual", 4)
 
 cars = [car, petrol_car]
